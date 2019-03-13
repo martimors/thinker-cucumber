a23e0255dc69df6614fed0f75c253e72355761af
Martin Morset <mmorset@gmail.com>
2019-03-13 21:10:31 +0100

    aaa

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'VeganPower4Eva'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
